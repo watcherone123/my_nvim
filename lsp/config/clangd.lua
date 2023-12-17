02ebba080c2d63e97255d5837df45cf8d2c84a21
watcherone123 <watcherone123@gmail.com>
2023-12-17 22:21:43 +0800

    add neogit

--- lsp/config/clangd.lua
+++ lsp/config/clangd.lua
@@ -2,4 +2,33 @@ return {
   capabilities = {
      offsetEncoding = "utf-8",
   },
+  memory_usage = {
+    border = vim.g.border_style,
+  },
+  symbol_info = {
+    border = vim.g.border_style,
+  },
+  inlay_hints = {
+    highlight = 'InlayHint',
+  },
+  ast = {
+    -- Use codicons
+    role_icons = {
+      type = '',
+      declaration = '',
+      expression = '',
+      specifier = '',
+      statement = '',
+      ['template argument'] = '',
+    },
+    kind_icons = {
+      Compound = '',
+      Recovery = '',
+      TranslationUnit = '',
+      PackExpansion = '',
+      TemplateTypeParm = '',
+      TemplateTemplateParm = '',
+      TemplateParamObject = '',
+    },
+  },
 }
---
+++ plugins/neogit/init.lua
@@ -0,0 +1,47 @@
+local utils = require "astronvim.utils"
+local prefix = "<leader>g"
+local icon = vim.g.icons_enabled and "󰰔 " or ""
+utils.set_mappings { n = { [prefix] = { desc = icon .. "Neogit" } } }
+
+local fold_signs = { utils.get_icon "FoldClosed", utils.get_icon "FoldOpened" }
+return {
+  {
+    "NeogitOrg/neogit",
+    dependencies = {
+      {
+        "nvim-lua/plenary.nvim",
+        "sindrets/diffview.nvim", -- optional - Diff integration
+
+        -- Only one of these is needed, not both.
+        "nvim-telescope/telescope.nvim", -- optional
+        "ibhagwan/fzf-lua",              -- optional
+      },
+    },
+    event = "User AstroGitFile",
+    opts = function(_, opts)
+      local disable_builtin_notifications = utils.is_available "nvim-notify" or utils.is_available "noice.nvim"
+
+      return utils.extend_tbl(opts, {
+        disable_builtin_notifications = disable_builtin_notifications,
+        telescope_sorter = function()
+          if utils.is_available "telescope-fzf-native.nvim" then
+            return require("telescope").extensions.fzf.native_fzf_sorter()
+          end
+        end,
+        integrations = { telescope = utils.is_available "telescope.nvim" },
+        signs = { section = fold_signs, item = fold_signs },
+      })
+    end,
+    keys = {
+      { prefix .. "nt", "<cmd>Neogit<CR>",        desc = "Open Neogit Tab Page" },
+      { prefix .. "nc", "<cmd>Neogit commit<CR>", desc = "Open Neogit Commit Page" },
+      { prefix .. "nd", ":Neogit cwd=",           desc = "Open Neogit Override CWD" },
+      { prefix .. "nk", ":Neogit kind=",          desc = "Open Neogit Override Kind" },
+    },
+  },
+  {
+    "catppuccin/nvim",
+    optional = true,
+    opts = { integrations = { neogit = true } },
+  },
+}
